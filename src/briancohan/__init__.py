91a618282fac1eccbf1db9232420e0b355cec26b
Brian Cohan <briancohan@gmail.com>
2024-01-18 16:33:46 -0500

    enable jinja globals for site nav and other configurations

--- src/briancohan/__init__.py
+++ src/briancohan/__init__.py
@@ -2,12 +2,21 @@ import os
 
 from flask import Flask, render_template
 
+from .config import Config
+
 app = Flask(__name__)
 
 if not os.environ.get("FLASK_ENV") == "development":
     from werkzeug.middleware.proxy_fix import ProxyFix
 
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+
+app.jinja_env.globals.update(
+    {
+        "SITE_NAV": Config.SITE_NAVIGATION,
+    }
+)
 
 
 @app.route("/")
---
+++ src/briancohan/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SITE_NAVIGATION = [
+        {"name": "Home", "url_for": "index"},
+        {"name": "About", "url_for": "about"},
+    ]
